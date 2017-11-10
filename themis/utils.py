3539139c4282c06e0d72eca39d5add589a79997d
Martastain <martas@imm.cz>
2017-11-10 23:06:37 +0100

    added utils.py

---
+++ themis/utils.py
@@ -0,0 +1,14 @@
+cuvid_decoders = {
+    "h264" : "h264_cuvid",
+    "hevc" : "hevc_cuvid",
+    "mjpeg" : "mjpeg_cuvid",
+    "mpeg1video" : "mpeg1_cuvid",
+    "mpeg2video" : "mpeg2_cuvid",
+    "mpeg4" : "mpeg4_cuvid",
+    "vc1" : "vc1_cuvid",
+    "vp8" : "vp8_cuvid",
+    "vp9" : "vp9_cuvid",
+}
+
+class ThemisProgress(dict):
+    pass
