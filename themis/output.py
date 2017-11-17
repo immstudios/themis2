523e6a33126a989c68cd47ad5fa4a5659314b908
Martastain <martas@imm.cz>
2017-11-17 22:19:43 +0100

    has_video and has_audio properties

--- themis/output.py
+++ themis/output.py
@@ -12,8 +12,6 @@ class ThemisOutput(object):
                 "width" : 1920,
                 "height" : 1080,
                 "fps" : 25,
-                "video_codec" : "h264",
-                "audio_codec" : "aac",
                 "audio_sample_rate" : "48000"
             }
         self.args.update(kwargs)
@@ -22,6 +20,14 @@ class ThemisOutput(object):
             for key in defaults:
                 if not key in self.args:
                     self.args[key] = defaults[key]
+
+    @property
+    def has_video(self):
+        return self["video_codec"] is not None
+
+    @property
+    def has_audio(self):
+        return self["audio_codec"] is not None
 
     def __getitem__(self, key):
         return self.args.get(key, None)
@@ -34,46 +40,48 @@ class ThemisOutput(object):
         # Video encoding profile
         #
 
-        if self["video_codec"] == "h264":
-            result.extend([
-                    "-pix_fmt", self["pixel_format"],
-                    "-b:v", self["video_bitrate"],
-                    "-g", self["gop_size"],
-                    "-preset:v", self["video_preset"],
-                    "-profile:v", self["video_profile"]
-                ])
-
-            if has_nvidia:
+        if self.has_video:
+            if self["video_codec"] == "h264":
                 result.extend([
-                        "-c:v", "h264_nvenc",
-                        "-strict_gop", "1",
-                        "-no-scenecut", "1",
-                    ])
-            else:
-                result.extend([
-                        "-c:v", "libx264",
-                        "-x264opts", "keyint={}:min-keyint={}:scenecut=-1".format(self["gop_size"], self["gop_size"])
+                        "-pix_fmt", self["pixel_format"],
+                        "-b:v", self["video_bitrate"],
+                        "-g", self["gop_size"],
+                        "-preset:v", self["video_preset"],
+                        "-profile:v", self["video_profile"]
                     ])
 
-        elif self["video_codec"] == "dnxhd":
-            result.extend([
-                    "-pix_fmt", self["pixel_format"],
-                    "-c:v", "dnxhd"
-                ])
+                if has_nvidia:
+                    result.extend([
+                            "-c:v", "h264_nvenc",
+                            "-strict_gop", "1",
+                            "-no-scenecut", "1",
+                        ])
+                else:
+                    result.extend([
+                            "-c:v", "libx264",
+                            "-x264opts", "keyint={}:min-keyint={}:scenecut=-1".format(self["gop_size"], self["gop_size"])
+                        ])
+
+            elif self["video_codec"] == "dnxhd":
+                result.extend([
+                        "-pix_fmt", self["pixel_format"],
+                        "-c:v", "dnxhd"
+                    ])
 
         #
         # Audio encoding profile
         #
 
-        result.extend([
-                "-c:a", self["audio_codec"],
-                "-ar", self["audio_sample_rate"]
-            ])
-
-        if self["audio_bitrate"]:
+        if self.has_audio:
             result.extend([
-                    "-b:a", self["audio_bitrate"]
+                    "-c:a", self["audio_codec"],
+                    "-ar", self["audio_sample_rate"]
                 ])
+
+            if self["audio_bitrate"]:
+                result.extend([
+                        "-b:a", self["audio_bitrate"]
+                    ])
 
         return result
 
